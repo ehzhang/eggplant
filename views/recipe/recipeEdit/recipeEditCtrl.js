7b79b5040c22aace47a9124276835527bfb50cf0
Edwin Zhang <ehzhang@mit.edu>
2016-05-01 15:37:36 -0400

    routing for edit

--- views/recipe/recipeEdit/recipeEditCtrl.js
+++ views/recipe/recipeEdit/recipeEditCtrl.js
@@ -1,13 +1,22 @@
 angular.module('app')
   .controller('RecipeEditCtrl',
-    function($scope, RecipeService, IngredientService) {
+    function($state, $scope, RecipeService, IngredientService) {
       // Set the 'Recipe' in this child scope to a clone of the parent scope's
       // Looks pretty fucky, I know.
       $scope.recipe = $.extend(true, {}, $scope.recipe);
+      $scope.recipe.changeSummary = "";
+
       $scope.ingredients = IngredientService.getAll();
 
       $scope.save = function() {
         // TODO: Still need to update version
         RecipeService.update($scope.recipe._id, $scope.recipe);
-      }
+        $state.go('app.recipe', {}, {
+          reload: true
+        });
+      };
+
+      $scope.cancel = function() {
+        $state.go('app.recipe');
+      };
     });
