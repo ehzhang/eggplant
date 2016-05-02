f14f627890b6c8936d0f6d418ecb7a434257f309
Edwin Zhang <ehzhang@mit.edu>
2016-05-02 16:22:09 -0400

    incoming garbage autocomplete box

--- views/recipe/recipeEdit/recipeEditCtrl.js
+++ views/recipe/recipeEdit/recipeEditCtrl.js
@@ -10,14 +10,58 @@ angular.module('app')
 
       $scope.newIngredient = {};
 
+      // LOOK AWAY, jQUERY DISASTER AHEAD
+      // This is because semantic-ui dropdown doesn't have a nice way to
+      // deal with a default value that should always appear in the dropdown.
+      function dropdownAddCreateNew(value) {
+        var text = "Create New";
+
+        if (value && value.length > 0){
+          text += "'" + value + "'";
+        }
+
+        if ($('#ingr-dropdown .button').length > 0) {
+          $('#ingr-dropdown .button').text(text);
+          return;
+        }
+
+        var $createNew = $("<div class='ui fluid purple button'>"+ text +"</div>");
+        $createNew.click(function(){
+          var val = $('#ingr-dropdown input.search').val();
+          alert(val);
+        });
+
+        $('#ingr-dropdown .menu').append($createNew);
+      }
+
+      $('#ingr-dropdown').dropdown({
+        duration: 0,
+        onChange: function(value, text, choice) {
+          $scope.newIngredient.name = value;
+        },
+        onNoResults: function(value) {
+          dropdownAddCreateNew(value);
+        }
+      });
+
+      $('#ingr-dropdown input.search').on('keyup', function(e){
+        var value = e.target.value;
+        dropdownAddCreateNew(value);
+      });
+
+      // Initialize the create new button
+      dropdownAddCreateNew();
+      // END DISASTER
+
       $scope.deleteIngredient = function (index) {
         $scope.recipe.ingredients.splice(index, 1);
-      }
+      };
 
       $scope.addNewIngredient = function() {
         // TODO: Validation
         $scope.recipe.ingredients.push($.extend(true, {}, $scope.newIngredient));
         $scope.newIngredient = {};
+        $('#ingr-dropdown').dropdown('clear');
       };
 
       $scope.save = function() {
