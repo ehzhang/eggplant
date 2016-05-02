51462c73226681afa83183fa1e6d5aa99c1e0f2d
landmann <nlandmanc@gmail.com>
2016-05-02 15:38:14 -0400

    Added sorting

--- views/browse/browseCtrl.js
+++ views/browse/browseCtrl.js
@@ -2,8 +2,13 @@ angular.module('app')
   .controller('BrowseCtrl',
     function($scope, $state, RecipeService){
       $scope.recipes = RecipeService.getAll();
-
+      $('.ui.dropdown')
+      .dropdown()
+      ;
       $scope.selectRecipe = function(id) {
         $state.go('app.recipe', {id: id});
       };
+      $scope.orderBy = function(x) {
+      	$scope.orderByInput = x;
+    }
     });
