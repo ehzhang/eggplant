aed8a532d78dd0d0dc6c67fb8831ee19f1e07264
Kate Yu <katexyu@gmail.com>
2016-05-02 16:00:49 -0400

    Add history

--- src/init.js
+++ src/init.js
@@ -1,5 +1,5 @@
 angular.module('app')
-  .run(function(RecipeService, IngredientService) {
+  .run(function(RecipeService, IngredientService, VersionService) {
     var DAY = 1000 * 60 * 60 * 24;
     var recipes = [
       {
@@ -162,6 +162,239 @@ angular.module('app')
       },
     ];
 
+    var versions = [{
+        recipeId: "0",
+        index: 0,
+        snapshot: {
+            id: "0",
+            name: "Eggplant Po Boy",
+            changeSummary: "Changed breading to panko.",
+            latestVersion: "1.2",
+            imageUrl: "assets/eggplant_poboy.jpg",
+            lastUpdated: new Date() - DAY,
+            description: "Man braid fixie farm-to-table pug wayfarers scenester before they sold out ramps tilde, man bun YOLO slow-carb squid shoreditch put a bird on it. Tote bag venmo lo-fi kombucha synth semiotics. Photo booth thundercats hella squid church-key semiotics. Scenester dreamcatcher cornhole, whatever affogato roof party 3 wolf moon kinfolk shabby chic meh twee photo booth irony keffiyeh. Literally chia jean shorts, viral kinfolk pickled bicycle rights flexitarian twee marfa neutra cray.",
+            ingredients: [{
+                name: "eggplant",
+                amount: 2,
+                units: "slice",
+            }, {
+                name: "quinoa",
+                amount: 1,
+                units: "cup",
+            }, {
+                name: "mixed greens",
+                amount: 0.5,
+                units: "cup",
+            }, {
+                name: "mayo",
+                amount: 2,
+                units: "tbsp",
+            }, {
+                name: "secret sauce",
+                amount: 0.5,
+                units: "tbsp",
+            }],
+            instructions: "1. Slice Eggplant \n2. Dip eggplant in egg and then dip in panko breadcrumbs \n3. Fry in oil about 5 minutes until golden brown \n4. Put in hoagie roll.",
+        },
+    }, {
+        recipeId: "1",
+        index: 0,
+        snapshot: {
+            id: "1",
+            name: "Eggplant Tacos",
+            changeSummary: "Used fresher tortillas.",
+            latestVersion: "2.5",
+            imageUrl: "assets/eggplant_taco.jpg",
+            lastUpdated: new Date() - 2 * DAY,
+            description: "Man braid fixie farm-to-table pug wayfarers scenester before they sold out ramps tilde, man bun YOLO slow-carb squid shoreditch put a bird on it. Tote bag venmo lo-fi kombucha synth semiotics. Photo booth thundercats hella squid church-key semiotics. Scenester dreamcatcher cornhole, whatever affogato roof party 3 wolf moon kinfolk shabby chic meh twee photo booth irony keffiyeh. Literally chia jean shorts, viral kinfolk pickled bicycle rights flexitarian twee marfa neutra cray.",
+            ingredients: [{
+                name: "eggplant",
+                amount: 2,
+                units: "slice",
+            }, {
+                name: "quinoa",
+                amount: 1,
+                units: "cup",
+            }, {
+                name: "mixed greens",
+                amount: 0.5,
+                units: "cup",
+            }, {
+                name: "mayo",
+                amount: 2,
+                units: "tbsp",
+            }, {
+                name: "secret sauce",
+                amount: 0.5,
+                units: "tbsp",
+            }],
+            instructions: "1. Slice the Eggplant \n2. Marinate Eggplant in secret sauce **30 minutes!** \n3. Cook Quinoa \n4. Toss Eggplant, mixed greens, and quinoa",
+          },
+    }, {
+        recipeId: "2",
+        index: 0,
+        snapshot: {
+            id: "0",
+            name: "Eggplant Quinoa Bowl",
+            changeSummary: "First version!",
+            latestVersion: "0.1",
+            imageUrl: "assets/eggplant_quinoa_bowl.jpg",
+            lastUpdated: new Date() - DAY*20,
+            description: "Yummy eggplant bowl",
+            ingredients: [{
+                name: "eggplant",
+                amount: 2,
+                units: "slice",
+            }, {
+                name: "quinoa",
+                amount: 1,
+                units: "cup",
+            }, {
+                name: "mixed greens",
+                amount: 0.5,
+                units: "cup",
+            }],
+            instructions: "1. Slice the Eggplant \n2. Marinate Eggplant in secret sauce **30 minutes!** \n3. Cook Quinoa \n4. Toss Eggplant, mixed greens, and quinoa",
+        }
+    }, {
+        recipeId: "2",
+        index: 1,
+        snapshot: {
+            id: "0",
+            name: "Eggplant Quinoa Bowl",
+            changeSummary: "Added secret sauce and mayo.",
+            latestVersion: "0.2",
+            imageUrl: "assets/eggplant_quinoa_bowl.jpg",
+            lastUpdated: new Date() - DAY*15,
+            description: "Yummy eggplant bowl",
+            ingredients: [{
+                name: "eggplant",
+                amount: 2,
+                units: "slice",
+            }, {
+                name: "quinoa",
+                amount: 1,
+                units: "cup",
+            }, {
+                name: "mixed greens",
+                amount: 0.5,
+                units: "cup",
+            }, {
+                name: "mayo",
+                amount: 2,
+                units: "tbsp",
+            }, {
+                name: "secret sauce",
+                amount: 0.25,
+                units: "tbsp",
+            }],
+            instructions: "1. Slice the Eggplant \n2. Marinate Eggplant in secret sauce **30 minutes!** \n3. Cook Quinoa \n4. Toss Eggplant, mixed greens, and quinoa",
+        }
+    }, {
+        recipeId: "2",
+        index: 2,
+        snapshot: {
+            id: "2",
+            name: "Eggplant Quinoa Bowl",
+            changeSummary: "Doubled the amount of secret sauce.",
+            latestVersion: "2.3",
+            imageUrl: "assets/eggplant_quinoa_bowl.jpg",
+            lastUpdated: new Date() - 3 * DAY,
+            description: "Man braid fixie farm-to-table pug wayfarers scenester before they sold out ramps tilde, man bun YOLO slow-carb squid shoreditch put a bird on it. Tote bag venmo lo-fi kombucha synth semiotics. Photo booth thundercats hella squid church-key semiotics. Scenester dreamcatcher cornhole, whatever affogato roof party 3 wolf moon kinfolk shabby chic meh twee photo booth irony keffiyeh. Literally chia jean shorts, viral kinfolk pickled bicycle rights flexitarian twee marfa neutra cray.",
+            ingredients: [{
+                name: "eggplant",
+                amount: 2,
+                units: "slice",
+            }, {
+                name: "quinoa",
+                amount: 1,
+                units: "cup",
+            }, {
+                name: "mixed greens",
+                amount: 0.5,
+                units: "cup",
+            }, {
+                name: "mayo",
+                amount: 2,
+                units: "tbsp",
+            }, {
+                name: "secret sauce",
+                amount: 0.5,
+                units: "tbsp",
+            }],
+            instructions: "1. Slice the Eggplant \n2. Marinate Eggplant in secret sauce **30 minutes!** \n3. Cook Quinoa \n4. Toss Eggplant, mixed greens, and quinoa",
+        }
+    }, {
+        recipeId: "3",
+        index: 0,
+        snapshot: {
+            id: "3",
+            name: "Eggplant Burrito",
+            changeSummary: "Whole wheat tortillas, locally made.",
+            latestVersion: "4.5",
+            imageUrl: "assets/eggplant_burrito.jpg",
+            lastUpdated: new Date() - 7 * DAY,
+            description: "Man braid fixie farm-to-table pug wayfarers scenester before they sold out ramps tilde, man bun YOLO slow-carb squid shoreditch put a bird on it. Tote bag venmo lo-fi kombucha synth semiotics. Photo booth thundercats hella squid church-key semiotics. Scenester dreamcatcher cornhole, whatever affogato roof party 3 wolf moon kinfolk shabby chic meh twee photo booth irony keffiyeh. Literally chia jean shorts, viral kinfolk pickled bicycle rights flexitarian twee marfa neutra cray.",
+            ingredients: [{
+                name: "eggplant",
+                amount: 2,
+                units: "slice",
+            }, {
+                name: "quinoa",
+                amount: 1,
+                units: "cup",
+            }, {
+                name: "mixed greens",
+                amount: 0.5,
+                units: "cup",
+            }, {
+                name: "mayo",
+                amount: 2,
+                units: "tbsp",
+            }, {
+                name: "secret sauce",
+                amount: 0.5,
+                units: "tbsp",
+            }],
+            instructions: "1. Slice the Eggplant \n2. Marinate Eggplant in secret sauce **30 minutes!** \n3. Cook Quinoa \n4. Toss Eggplant, mixed greens, and quinoa",
+          },
+    }, {
+        recipeId: "4",
+        index: 0,
+        snapshot: {
+            id: "4",
+            name: "Eggplant Fritters",
+            changeSummary: "Now using a beer batter.",
+            latestVersion: "3.2",
+            imageUrl: "assets/eggplant_fritter.jpg",
+            lastUpdated: new Date() - 15 * DAY,
+            description: "Man braid fixie farm-to-table pug wayfarers scenester before they sold out ramps tilde, man bun YOLO slow-carb squid shoreditch put a bird on it. Tote bag venmo lo-fi kombucha synth semiotics. Photo booth thundercats hella squid church-key semiotics. Scenester dreamcatcher cornhole, whatever affogato roof party 3 wolf moon kinfolk shabby chic meh twee photo booth irony keffiyeh. Literally chia jean shorts, viral kinfolk pickled bicycle rights flexitarian twee marfa neutra cray.",
+            ingredients: [{
+                name: "eggplant",
+                amount: 2,
+                units: "slice",
+            }, {
+                name: "quinoa",
+                amount: 1,
+                units: "cup",
+            }, {
+                name: "mixed greens",
+                amount: 0.5,
+                units: "cup",
+            }, {
+                name: "mayo",
+                amount: 2,
+                units: "tbsp",
+            }, {
+                name: "secret sauce",
+                amount: 0.5,
+                units: "tbsp",
+            }],
+            instructions: "1. Slice the Eggplant \n2. Marinate Eggplant in secret sauce **30 minutes!** \n3. Cook Quinoa \n4. Toss Eggplant, mixed greens, and quinoa",
+
+          },
+    }];
+
     if (RecipeService.getAll().length === 0) {
       recipes.forEach(function(r) {
         RecipeService.add(r);
@@ -195,6 +428,12 @@ angular.module('app')
     if (IngredientService.getAll().length === 0) {
         ingredients.forEach(function(i){
             IngredientService.add(i);
+        });
+    }
+
+    if (VersionService.getAll().length === 0) {
+        versions.forEach(function(i){
+            VersionService.add(i);
         });
     }
   });
--- views/recipe/recipeHistory/recipeHistoryCtrl.js
+++ views/recipe/recipeHistory/recipeHistoryCtrl.js
@@ -1,5 +1,6 @@
 angular.module('app')
   .controller('RecipeHistoryCtrl',
-    function($scope) {
-
+    function($scope, VersionService) {
+        $scope.versions = VersionService.getAllForRecipe($scope.recipe.id);
+        console.log($scope.versions);
     });
