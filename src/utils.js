ae8607ca0e0d9d610db6274c5edc29b4663ccefc
Rachel Wang <rswang@mit.edu>
2016-05-04 12:18:23 -0400

    do sketchy things for stats

--- src/utils.js
+++ src/utils.js
@@ -3,6 +3,9 @@ angular.module('app')
     $rootScope.UTIL = {
       fromNow: function(time) {
         return moment(time).fromNow();
+      },
+      formatDate: function(time) {
+        return moment(time).format('MM/DD/YY');
       }
     };
   });
--- views/recipe/recipeStats/recipeStatsCtrl.js
+++ views/recipe/recipeStats/recipeStatsCtrl.js
@@ -3,6 +3,7 @@ angular.module('app')
     function($scope, $state, VersionService) {
         Chart.defaults.global.defaultFontFamily = 'Avenir Next';
 
+
         if ($('#ingr-dropdown .button').length > 0) {
           $('#ingr-dropdown .button').text(text);
           return;
@@ -22,11 +23,34 @@ angular.module('app')
             return 500 + Math.round(Math.random() * 100);
         };
 
+        function formatDate(time) {
+            return moment(time).format('MM/DD/YY');
+        }
+
+        function zeroDate(date) {
+            return moment(date).startOf('date');
+        }
         var versions = VersionService.getAllForRecipe($scope.recipe.id);
+
         $scope.versions = versions.sort(function(a, b){
             return a.snapshot.lastUpdated - b.snapshot.lastUpdated;
         });
+        var minDate = $scope.versions[0].snapshot.lastUpdated;
+        var maxDate = new Date();
+        maxDate.setHours(0);
+        maxDate.setMinutes(0);
+        maxDate.setSeconds(0);
+        maxDate.setMilliseconds(0);
 
+        $('#daterange').daterangepicker({
+            format: 'MM/DD/YY',
+            startDate: formatDate(minDate),
+            endDate: formatDate(maxDate),
+            minDate: formatDate(minDate),
+            maxDate: formatDate(maxDate),
+        }, function(start, end, label) {
+            drawChart(zeroDate(start), zeroDate(end));
+        });
 
 
         function getDiff(version) {
@@ -47,169 +71,218 @@ angular.module('app')
 
         for (var i = 0; i < $scope.versions.length; i++) {
             var currentDate = new Date($scope.versions[i].snapshot.lastUpdated);
-            var nextDate = new Date();
+            var nextDate = maxDate;
             if (i < $scope.versions.length - 1) {
                 var nextDate = new Date($scope.versions[i+1].snapshot.lastUpdated);
             }
             var randomData = randomScalingFactor();
             versionData.push({
-                x: new Date(currentDate),
+                x: new Date(zeroDate(currentDate)),
                 y: randomData,
+                version: $scope.versions[i].snapshot.latestVersion,
             }); 
             salesData.push({
-                x: new Date(currentDate),
+                x: new Date(zeroDate(currentDate)),
                 y: randomData,
             }); 
             currentDate.setDate(currentDate.getDate() + 1);
 
             while (currentDate < nextDate) {
                 salesData.push({
-                    x: new Date(currentDate),
+                    x: new Date(zeroDate(currentDate)),
                     y: randomScalingFactor(),
                 }); 
                 currentDate.setDate(currentDate.getDate() + 1);
             }
         }
-        versionData.push({
-            x: new Date(),
-            y: versionData[versionData.length - 1].y,
-        }); 
+        
+        var drawChart = function(startDate, endDate) {
+            $("#canvas").replaceWith("<canvas id='canvas'></canvas>");
+            $("#canvas").on("mouseover", null);
+            var ctx = document.getElementById("canvas").getContext("2d");
 
-        var data = {
-            datasets: [
-                {
-                    label: "Versions",
-                    data: versionData,
-                    borderColor: 'rgba(155, 89, 182, .9)',
-                    pointBorderColor: 'rgba(155, 89, 182, 1)',
-                    pointBackgroundColor: 'rgba(155, 89, 182, 1)',
-                }, {
-                    label: "Sales",
-                    data: salesData,
-                    borderColor: 'rgba(155, 89, 182, .3)',
-                    pointBorderColor: 'rgba(155, 89, 182, .3)',
-                    pointBackgroundColor: 'rgba(155, 89, 182, .3)',
-                    borderDash: [10,5],
-                }
-            ]
-        };
+            var filteredVersionData = versionData.filter(function(d) {
+                return moment(d.x).isSameOrAfter(new Date(startDate), 'date') && moment(d.x).isSameOrBefore(new Date(endDate), 'date');
+            });
+            var filteredSalesData = salesData.filter(function(d) {
+                return moment(d.x).isSameOrAfter(new Date(startDate), 'date') && moment(d.x).isSameOrBefore(new Date(endDate), 'date');
+            });
 
-        data.datasets.forEach(function(dataset) {
-            dataset.borderWidth = 6;
-            dataset.backgroundColor = 'rgba(155, 89, 182,.05)';
-            dataset.pointHoverRadius = 6;
-            dataset.radius = 6;
-            dataset.hitRadius = 20;
-            dataset.pointBorderWidth = 3;
-        });
+            if (!moment(filteredVersionData[0].x).isSame(new Date(startDate), 'date')) {
+                filteredVersionData.unshift({
+                    x: new Date(startDate),
+                    y: filteredSalesData[0].y,
+                    version: filteredVersionData[0].version - 1,
+                })
+            }
 
-        var ctx = document.getElementById("canvas").getContext("2d");
+            if (!moment(filteredVersionData[filteredVersionData.length - 1].x).isSame(new Date(endDate), 'date')) {
+                filteredVersionData.push({
+                    x: new Date(zeroDate(endDate)),
+                    y: filteredSalesData[filteredSalesData.length - 1].y,
+                    version: versionData[versionData.length - 1].version + 1,
+                });
+            }
 
-        var chart = new Chart(ctx, {
-            type: 'line',
-            data: data,
-            options: {
-                hover: {
-                    mode: 'label'
-                },
-                title: {
-                    display: false,
-                    text: $scope.recipe.name + ' Sales'
-                },
-                scales: {
-                    xAxes: [{
-                        type: "time",
-                        position: 'bottom',
-                        scaleLabel: {
-                            display: true,
-                            labelString: 'Date'
-                        },
-                        time: {
-                            // Moment js for each of the units. Replaces `displayFormat`
-                            // To override, use a pattern string from http://momentjs.com/docs/#/displaying/format/
-                            displayFormats: {
-                                'millisecond': 'SSS [ms]',
-                                'second': 'h:mm:ss a', // 11:20:01 AM
-                                'minute': 'h:mm:ss a', // 11:20:01 AM
-                                'hour': 'MMM D, hA', // Sept 4, 5PM
-                                'day': 'll', // Sep 4 2015
-                                'week': 'll', // Week 46, or maybe "[W]WW - YYYY" ?
-                                'month': 'MMM YYYY', // Sept 2015
-                                'quarter': '[Q]Q - YYYY', // Q3
-                                'year': 'YYYY', // 2015
+            var data = {
+                datasets: [
+                    {
+                        label: "Versions",
+                        data: filteredVersionData,
+                        borderColor: 'rgba(155, 89, 182, .9)',
+                        pointBorderColor: 'rgba(155, 89, 182, 1)',
+                        pointBackgroundColor: 'rgba(155, 89, 182, 1)',
+                    }, {
+                        label: "Sales",
+                        data: filteredSalesData,
+                        borderColor: 'rgba(155, 89, 182, .3)',
+                        pointBorderColor: 'rgba(155, 89, 182, .3)',
+                        pointBackgroundColor: 'rgba(155, 89, 182, .3)',
+                        borderDash: [10,5],
+                    }
+                ]
+            };
+
+            data.datasets.forEach(function(dataset) {
+                dataset.borderWidth = 6;
+                dataset.backgroundColor = 'rgba(155, 89, 182,.05)';
+                dataset.pointHoverRadius = 6;
+                dataset.radius = 6;
+                dataset.hitRadius = 20;
+                dataset.pointBorderWidth = 3;
+            });
+
+
+            var chart = new Chart(ctx, {
+                type: 'line',
+                data: data,
+                options: {
+                    hover: {
+                        mode: 'label'
+                    },
+                    title: {
+                        display: false,
+                        text: $scope.recipe.name + ' Sales'
+                    },
+                    scales: {
+                        xAxes: [{
+                            type: "time",
+                            position: 'bottom',
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Date'
                             },
-                            // Sets the display format used in tooltip generation
-                            tooltipFormat: 'll',
-                        },
-                    }],
-                    yAxes: [{
-                        position: 'left',
-                        scaleLabel: {
-                            display: true,
-                            labelString: 'Sales'
-                        }
-                    }],
-                },
-                tooltips: {
-                    callbacks: {
-                        label: function(tooltipItem, data) {
-                            if (tooltipItem.datasetIndex == 0) {
-                                return "Updated to Version " + parseInt(tooltipItem.datasetIndex + 1);
-                            } else {
-                                return "Sales: " + tooltipItem.yLabel;
+                            time: {
+                                // Moment js for each of the units. Replaces `displayFormat`
+                                // To override, use a pattern string from http://momentjs.com/docs/#/displaying/format/
+                                displayFormats: {
+                                    'millisecond': 'SSS [ms]',
+                                    'second': 'h:mm:ss a', // 11:20:01 AM
+                                    'minute': 'h:mm:ss a', // 11:20:01 AM
+                                    'hour': 'MMM D, hA', // Sept 4, 5PM
+                                    'day': 'll', // Sep 4 2015
+                                    'week': 'll', // Week 46, or maybe "[W]WW - YYYY" ?
+                                    'month': 'MMM YYYY', // Sept 2015
+                                    'quarter': '[Q]Q - YYYY', // Q3
+                                    'year': 'YYYY', // 2015
+                                },
+                                // Sets the display format used in tooltip generation
+                                tooltipFormat: 'll',
+                                // unit: 'day',
+
+                            },
+                            ticks: {
+                                min: startDate,
+                                max: endDate,
+                                determineDataLimits: function() {
+                                    this.max = endDate;
+                                    this.min = startDate;
+                                },
+
+                            }
+                        }],
+                        yAxes: [{
+                            position: 'left',
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Sales'
+                            },
+                            ticks: {
+                            }
+
+                        }],
+                    },
+                    tooltips: {
+                        callbacks: {
+                            label: function(tooltipItem, data) {
+                                if (tooltipItem.datasetIndex == 0) {
+                                    var updatedVersion = parseInt(data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index].version);
+                                    if (updatedVersion == 1) {
+                                        return "Created Version 1";
+                                    } else if (updatedVersion >= filteredVersionData[filteredVersionData.length - 1].version) {
+                                        return "";
+                                    } else {
+                                        return "Updated to Version " + updatedVersion;
+                                    }
+                                } else {
+                                    return "Sales: " + tooltipItem.yLabel;
+                                }
                             }
                         }
-                    }
-                },
-            }
-        });
-
-        var xBins = chart.config.data.datasets[0].metaData.map(function(e) {
-            return e._view.x;
-        });
-
-        function findBin(x) {
-            if (x < xBins[0]) {
-                return -1;
-            } else {
-                var i = 0;
-                while (i < xBins.length) {
-                    if (x >= xBins[i] && x < xBins[i+1]) {
-                        return i;
-                    }
-                    i++;
+                    },
                 }
-                return xBins.length;
-            }
-        }
-
-        var resizePopup = function(){$('.ui.popup').css('max-height', $(window).height());};
-
-        $(window).resize(function(e){
-            resizePopup();
-        });
-
-        $("#canvas").on("mouseover", function(e) {
-            var bin = findBin(e.offsetX);
-            if (bin < 0 || bin >= xBins.length) {
-                return;
-            }
-            var rectWidth = xBins[bin+1] - xBins[bin];
-            var rectHeight = chart.chartArea.bottom - chart.chartArea.top;
-            $("#stats-highlighter").remove();
-            $("#canvas-container").append("<div id='stats-highlighter'></div>")
-            $("#stats-highlighter").css({
-                left: xBins[bin],
-                top: chart.chartArea.top,
-                width: rectWidth,
-                height: rectHeight,
             });
-            $("#stats-highlighter").popup({
-                position : 'right center',
-                hoverable: true,
-                popup : $('#popup-' + bin),
+
+            var xBins = chart.config.data.datasets[0].metaData.map(function(e) {
+                return e._view.x;
             });
-        });
+
+            function findBin(x) {
+                if (x < xBins[0]) {
+                    return -1;
+                } else {
+                    var i = 0;
+                    while (i < xBins.length) {
+                        if (x >= xBins[i] && x < xBins[i+1]) {
+                            return i;
+                        }
+                        i++;
+                    }
+                    return xBins.length;
+                }
+            }
+
+            var resizePopup = function(){$('.ui.popup').css('max-height', $(window).height());};
+
+            $(window).resize(function(e){
+                resizePopup();
+            });
+
+            $("#canvas").on("mouseover", function(e) {
+                var bin = findBin(e.offsetX);
+                if (bin < 0 || bin >= xBins.length) {
+                    return;
+                }
+                var rectWidth = xBins[bin+1] - xBins[bin];
+                var rectHeight = chart.chartArea.bottom - chart.chartArea.top;
+                $("#stats-highlighter").remove();
+                $("#canvas-container").append("<div id='stats-highlighter'></div>")
+                $("#stats-highlighter").css({
+                    left: xBins[bin],
+                    top: chart.chartArea.top,
+                    width: rectWidth,
+                    height: rectHeight,
+                });
+                $("#stats-highlighter").popup({
+                    position : 'right center',
+                    hoverable: true,
+                    popup : $('#popup-' + parseInt(filteredVersionData[bin].version - 1)),
+                });
+            });
+    }
+
+        
+    drawChart(minDate, maxDate);
 
 
     $scope.selectVersion = function(id) {
